3b0149d1f805d066a2ae49eb99388cb566f30fca
Ilya <aintnewtathis@mail.ru>
2024-04-22 13:38:23 +0300

    Eddited style link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
 	import { Toaster } from 'svelte-french-toast'
 </script>
 
+<svelte:head>
+	<link href="/styles/normalize.css" rel="stylesheet" />
+	<link href="/styles/global.scss" rel="stylesheet" />
+</svelte:head>
+	
+		
 <Toaster />
 <main>
 	<slot  />
